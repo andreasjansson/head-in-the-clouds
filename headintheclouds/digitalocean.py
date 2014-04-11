addeff2aae80b2f470801e3c6e11b474dc9090bc
andreas jansson <andreas.s.t.jansson@gmail.com>
2014-04-11 13:19:53 -0400

    allow deprecated images fix bug

--- headintheclouds/digitalocean.py
+++ headintheclouds/digitalocean.py
@@ -205,7 +205,7 @@ def _get_sizes():
 def _get_images():
     images = [s.to_json() for s in _do().images()]
     images = {s['name']: s['id'] for s in images}
-    for image_id, image_name in DEPRECATED_IMAGE_IDS:
+    for image_id, image_name in DEPRECATED_IMAGE_IDS.items():
         images[image_name] = image_id
     return images
 
