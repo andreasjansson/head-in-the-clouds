9e492fd74c20a647ebb0c4a7046adc3f67bb5e06
Andreas Jansson <andreasj@spotify.com>
2016-02-23 14:58:13 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='headintheclouds',
-      version='0.7.0',
+      version='0.7.1',
       description='Provider-agnostic cloud provisioning and Docker orchestration',
       author='Andreas Jansson',
       author_email='andreas@jansson.me.uk',
